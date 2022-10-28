dd3d3c7b05ee72bebc990ca33cbaed25f1b9ca15
Same Titan <49342907+loopopski@users.noreply.github.com>
2022-10-28 04:33:04 +0100

    Compresss before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-'''Fabric script to generate .tgz archive'''
-
+"""web server distribution
+    """
 from fabric.api import local
+import tarfile
+import os.path
+import re
 from datetime import datetime
 
-from fabric.decorators import runs_once
 
-
-@runs_once
 def do_pack():
-    '''generates .tgz archive from the contents of the web_static folder'''
-    local("mkdir -p versions")
-    path = ("versions/web_static_{}.tgz"
-            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
-    result = local("tar -cvzf {} web_static"
-                   .format(path))
-
-    if result.failed:
+    """distributes an archive to your web servers
+    """
+    target = local("mkdir -p versions")
+    name = str(datetime.now()).replace(" ", '')
+    opt = re.sub(r'[^\w\s]', '', name)
+    tar = local('tar -cvzf versions/web_static_{}.tgz web_static'.format(opt))
+    if os.path.exists("./versions/web_static_{}.tgz".format(opt)):
+        return os.path.normpath("/versions/web_static_{}.tgz".format(opt))
+    else:
         return None
-    return path
