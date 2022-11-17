6e98b9a6452fc14bd768b0f4d90018500591408e
Same Titan <49342907+loopopski@users.noreply.github.com>
2022-11-17 05:28:42 +0100

    Improve engine

--- models/state.py
+++ models/state.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
-"""
-    contains state class to represent a state
-"""
+""" Contains state class to represent a State """
 
 from models.base_model import BaseModel, Base
-import models
+import models import storage_type
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import environ
-
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
     """ State class: class to represent states of cities"""
-    if (storage_engine == 'db'):
-        __tablename__ = "states"
+    if storage_type == 'db':
+    __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+        cities = relationship('City', backref='state', cascade='all, delete, delete-orphan')
     else:
-        name = ""
+        name = ''
 
         @property
         def cities(self):
             """cities list
             """
-            result = []
-            for j, i in models.storage.all(models.city.City).items():
-                if (i.state_id == self.id):
-                    result.append(i)
-            return result
+            from models import storage
+            related_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id):
+                    related_cities.append(city)
+            return related_cities
